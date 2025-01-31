aa05ad4236ec0b1dbb5108236772eb32b7fc4f35
Roshanrobin80 <roshanrobin220303@gmail.com>
2025-01-31 10:45:00 +0530

    portfolio updated

--- prtfolio/main.py
+++ prtfolio/main.py
@@ -1,9 +1,9 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
-con = sqlite3.connect("c:/Users/My Pc/OneDrive/Documents/roshan/portfol/prtfolio/dbs.db")
+con = sqlite3.connect("dbs.db")
 try:
-    con.execute("create table std(name text, age int, email text)")
+    con.execute("create table msg(name text,email email,msg text)")
 except sqlite3.OperationalError:
     pass
 
@@ -13,3 +13,20 @@ app = Flask(__name__)
 def Home():
     return render_template('index.html')
 
+@app.route('/contact',methods=['GET','POST'])
+
+
+def contact():
+    if request.method == 'POST':
+        name=request.form.get('name')
+        email=request.form.get('email')
+        msg=request.form.get('msg')
+        print(name,email,msg)
+        con=sqlite3.connect('dbs.db')
+        con.execute("insert into msg(name,email,msg)values(?,?,?)",(name,email,msg))
+        con.commit()
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
